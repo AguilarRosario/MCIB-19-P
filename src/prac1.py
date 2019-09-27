ad016cb749ac75b56dfd185b85d9a51f3eef1e2c
Rosario <marosarioaguilar97@gmail.com>
2019-09-27 01:08:16 -0500

    Práctica 1
    Despliegue de registro de señales, aproximación de onda cuadrada y triangular

--- src/prac1.py
+++ src/prac1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Archivos
-sig = np.loadtxt('C:\\Users\HP desktop\Documents\Trimestre19P\MCIB\MCIB-19-P\data\Practica 1\Registro1Act1.txt')
+sig = np.loadtxt('C:\\Users\\HP desktop\\Documents\\Trimestre19P\\MCIB\MCIB-19-P\\data\\Practica 1\\Registro1Act1.txt')
 t1 = int(1./sig[1,0])
 t2 = int(1.25/sig[1,0])
 
@@ -15,9 +15,30 @@ ax3.plot(sig[t1:t2,0], sig[t1:t2,4]); ax3.set(title = 'Oximetro', ylabel='V', xl
 plt.show()
 fig.savefig('C:\\Users\HP desktop\Documents\Trimestre19P\MCIB\MCIB-19-P\images\Practica 1\p1.png')
 
-
 #Onda cuadrada
-k = 500
 sr = 300
 f = 5 
+t = np.linspace(0,1,sr)[np.newaxis]   #vector de tiempo 
 
+n = int(input("""Dame el número de armónicos: """))
+F = np.linspace(1,2*n-1,n)
+F = F[:,np.newaxis]
+
+# Aproximación de onda cuadrada
+S = F*(2*np.pi*t*f)
+S = 1/F*(np.sin(S))
+S = 4*(np.sum(S, axis=0))/np.pi
+
+fig,ax = plt.subplots()
+ax.plot(t[0,:],S); ax.set(xlabel='Tiempo [s]', ylabel='Amplitud', title='Aproximación de onda cuadrada para %d armónicos'%n)
+plt.show(block=False)
+
+# Aproximación de onda triangular 
+s2 = F*(2*np.pi*t*f)
+s2 = np.cos(s2)
+k = 1/(F**2)
+s2 = 8*np.sum(k*s2,axis=0)/np.pi**2
+
+fig,ax = plt.subplots()
+ax.plot(t[0,:],s2); ax.set(xlabel='Tiempo [s]', ylabel='Amplitud', title='Aproximación de onda triangular para %d armónicos'%n)
+plt.show()
