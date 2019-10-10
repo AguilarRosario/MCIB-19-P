0c44e6832ae11e2fe5ce69f0c78de84cea559c83
Toño SC <mouzhroq@gmail.com>
2019-10-09 20:28:01 -0500

    funcion filter

--- src/MCI.py
+++ src/MCI.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from scipy.fftpack import fft 
 from scipy.signal import windows
+import scipy.signal as sg
 
 def FFT (signal):
     f1 = 20*np.log10(np.abs(fft(signal)))
@@ -13,23 +14,33 @@ def FFT (signal):
     f3 = 20*np.log10(np.abs(fft(signal*w2)))
     f4 = 20*np.log10(np.abs(fft(signal*w3)))
     fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, sharex=True, constrained_layout=True)
-    ax0.plot(f1[0:int(len(f1)/2)]); ax0.set(title='FFT sin ventana', ylabel='dB')
-    ax1.plot(f2[0:int(len(f2)/2)]); ax1.set(title='FFT con ventana hann', ylabel='dB')
-    ax2.plot(f3[0:int(len(f3)/2)]); ax2.set(title='FFT con ventana hamming', ylabel='dB')
-    ax3.plot(f4[0:int(len(f4)/2)]); ax3.set(title='FFT con ventana blackman', ylabel='dB', xlabel='Frecuencia [Hz]')
+    ax0.plot(f1[0:len(f1)//2]); ax0.set(title='FFT sin ventana', ylabel='dB')
+    ax1.plot(f2[0:len(f2)//2]); ax1.set(title='FFT con ventana hann', ylabel='dB')
+    ax2.plot(f3[0:len(f3)//2]); ax2.set(title='FFT con ventana hamming', ylabel='dB')
+    ax3.plot(f4[0:len(f4)//2]); ax3.set(title='FFT con ventana blackman', ylabel='dB', xlabel='Frecuencia [Hz]')
 
     plt.show()
     return fig
 
 def psd(s, l, t, sr):
-    index = np.concatenate((np.arange(0, len(s)-l, l), np.delete((np.arange(0, len(s)-l, l)-(l*(t/100))),0))).astype(int)
+    x = np.arange(0, len(s)-l, l-1)
+    y = np.delete((np.arange(0, len(s)-l, l-1)-(l*(t/100))),0)
+    index = np.empty(len(x)+len(y)).astype(int)
+    index[::2] = x
+    index[1::2] = y
     epochs = np.array([s[i:i+l] for i in index])
-    w = np.linspace(0, sr, l)
-    mfft = (np.abs(fft(epochs)))
+    w = np.linspace(0, 2*sr, l)
+    mfft = (20*np.log10(np.abs(fft(epochs))))
     mfft = (1/len(epochs[:,0]))*np.sum(mfft, axis=0)
     fig, ax = plt.subplots()
-    ax.plot(w[0:int(len(mfft)/2)], mfft[0:int(len(mfft)/2)])
+    ax.plot(w[0:int(len(mfft)/2)], mfft[0:int(len(mfft)/2)]);ax.set(title='PSD', xlabel='Frecuencia (Hz)', ylabel='dB')
     plt.show()  
     return fig
 
+def filter(a, b, sr):
+    w, h = sg.freqz(b, a)
+    fig, ax =plt.subplots()
+    ax.plot(w*sr/np.pi, 20*np.log10(np.abs(h)))
+    plt.show()
+    return fig
 
--- src/prac2.py
+++ src/prac2.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+import scipy.signal as sg 
 import MCI 
 
 s = np.loadtxt('C:\\Users\\Mouzhroq\\Desktop\\Python\\MCIB-19-P\\data\\Practica 1\\Registro1Act1.txt')
@@ -10,8 +11,13 @@ plt.show(block=False)
 fig = MCI.FFT(s[:,2])
 fig.savefig('C:\\Users\\Mouzhroq\\Desktop\\Python\\MCIB-19-P\\images\\Practica 2\\pruebaFFT.png')
 
-sr = 60000
-l = 256
+sr = 1000
+l = 128
 tr = 25
 fig = MCI.psd(s[:,2], l, tr, sr)
 fig.savefig('C:\\Users\\Mouzhroq\\Desktop\\Python\\MCIB-19-P\\images\\Practica 2\\pruebaPSD.png')
+
+fc = 50
+b, a = sg.butter(10, fc/sr, btype='lowpass')
+fig = MCI.filter(a, b,sr)
+fig.savefig('C:\\Users\\Mouzhroq\\Desktop\\Python\\MCIB-19-P\\images\\Practica 2\\pruebafilter.png')
