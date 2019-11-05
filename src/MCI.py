723ef8015e38f31d10b966a3fe375ac7c51f28e8
Toño SC <mouzhroq@gmail.com>
2019-11-04 22:46:27 -0600

    Ejercicio 1

--- src/MCI.py
+++ src/MCI.py
@@ -19,8 +19,6 @@ def FFT (signal, sr):
     ax1.plot(w[0:len(signal)//2], f2[0:len(f2)//2]); ax1.set(title='FFT con ventana hann', ylabel='dB')
     ax2.plot(w[0:len(signal)//2], f3[0:len(f3)//2]); ax2.set(title='FFT con ventana hamming', ylabel='dB')
     ax3.plot(w[0:len(signal)//2], f4[0:len(f4)//2]); ax3.set(title='FFT con ventana blackman', ylabel='dB', xlabel='Frecuencia [Hz]')
-
-    # plt.show(block=False)
     return fig
 
 def psd(s, l, t, sr):
@@ -32,16 +30,12 @@ def psd(s, l, t, sr):
     w = np.linspace(0, 2*sr, l)
     mfft = (20*np.log10(np.abs(fft(epochs))))
     mfft = (1/len(epochs[:,0]))*np.sum(mfft, axis=0)
-    # fig, ax = plt.subplots()
-    # ax.plot(w[0:int(len(mfft)/2)], mfft[0:int(len(mfft)/2)]);ax.set(title='PSD', xlabel='Frecuencia (Hz)', ylabel='dB')
-    # plt.show()  
-    return mfft
+    return mfft, w
 
 def fil(a, b, sr):
     w, h = sg.freqz(b, a)
     fig, ax =plt.subplots()
     ax.plot(w*sr/np.pi, 20*np.log10(np.abs(h))); ax.set(title='Respuesta del filtro', xlabel='Frecuencia (Hz)', ylabel='dB')
-    #plt.show()
     return fig
 
 def media_movil(s, l, t, time):
@@ -57,10 +51,6 @@ def media_movil(s, l, t, time):
     for i in range (1, ti.shape[0]):
         filt = np.append(filt, ti[i, int(len(filt)-index[i])::])
     filt = np.append(filt, s[index[-1]+l::]-np.mean(s[index[-1]+l::]))
-    # fig, (ax0, ax1) = plt.subplots(nrows=2, constrained_layout=True, sharex=True)
-    # ax0.plot(time, s); ax0.set(title='Señal sin filtrar', ylabel='Voltaje')
-    # ax1.plot(time, filt); ax1.set(title='Señal filtrada', ylabel='Voltaje', xlabel='Tiempo')
-    # plt.show()
     return filt
 
 def detrend(s, time, l):
@@ -84,20 +74,36 @@ def detrend(s, time, l):
     y = s[index[-1]+l::]
     w = np.dot(np.dot(np.linalg.inv(np.dot(x, x.T)), x), y.T)
     filt = np.append(filt, s[index[-1]+l::]-(w[0]*time[index[-1]+l::]+w[1]))
-    # fig, (ax0, ax1) = plt.subplots(nrows=2, constrained_layout=True, sharex=True)
-    # ax0.plot(time, s); ax0.set(title='Señal sin filtrar', ylabel='Voltaje')
-    # ax1.plot(time, filt); ax1.set(title='Señal filtrada', ylabel='Voltaje', xlabel='Tiempo')
-    # plt.show()
     return filt
+
+def zscore(s):
+    media = (1/s.shape[1])*(np.sum(s, axis=1))
+    sigma = np.sqrt((1/(s.shape[1]-1))*(np.sum((s-media[:,np.newaxis])**2, axis=1)))
+    z     = (s-media[:,np.newaxis])/sigma[:,np.newaxis]
+    return z
 
 def histograma(s,nbins,width=0.5):
     intervals = np.linspace(s.min(),s.max(),nbins+1)
     intervals = intervals[np.newaxis]
     left  = s[:,np.newaxis]>intervals[0,:-1]
     right = s[:,np.newaxis]<intervals[0,1:]
-
     hist = (left*right).sum(axis=0)
-    # plt.figure()
-    # plt.bar( intervals[0,:-1], hist, width )
-    # plt.show()
     return intervals[0,:-1],hist
+
+def epochs(signal, segt, sr):
+    segt = segt*sr
+    i = 0
+    epochs = np.array([])
+    while(i<len(signal)-segt):
+        epochs = np.append(epochs, signal[i:i+segt])
+        i = i+segt
+    epochs = np.reshape(epochs, (int(len(signal)/segt),segt))
+    return epochs
+
+def coef_corr(signal1, signal2):
+    r = np.array([])
+    for i in range(signal1.shape[0]):
+        S = np.cov(signal1[i,:], signal2[i,:])
+        z = S[0,1]/np.product( np.sqrt(S.diagonal() ) )
+        r = np.append(r, z)
+    return r 
--- src/practica3.py
+++ src/practica3.py
@@ -22,10 +22,8 @@ ax1.plot(t, ECG); ax1.set(ylabel='ECG')
 ax2.plot(t, EMG); ax2.set(ylabel='EMG')
 ax3.plot(t, pulso); ax3.set(xlabel='Tiempo [seg]',ylabel='Pulso')
 fig.suptitle('Eliminación de tendencia lineal')
-#plt.show()
 
 # Filtrado de la señal 
- 
 b, a = signal.butter(4,2/sr*30)
 ECG_f  = signal.filtfilt(b,a,ECG)
 Resp_f  = signal.filtfilt(b,a,Resp)
@@ -37,7 +35,7 @@ ax1.plot(t, ECG_f); ax1.set(ylabel='ECG')
 ax2.plot(t, EMG); ax2.set(ylabel='EMG')
 ax3.plot(t, pulso_f); ax3.set(ylabel='Pulso', xlabel='Tiempo [seg]')
 fig.suptitle('Eliminación de tendencia lineal y filtrado')
-#plt.show()
+
 
 ini = np.array([])
 ini = np.append(ini, [Resp_f[0:15*sr], ECG_f[0:15*sr], EMG[0:15*sr], pulso_f[0:15*sr]]).reshape(4, 15000) 
@@ -48,12 +46,12 @@ eje = np.append(eje, [Resp_f[60*sr:75*sr], ECG_f[60*sr:75*sr], EMG[60*sr:75*sr],
 fin = np.array([])
 fin = np.append(fin, [Resp_f[90*sr:105*sr], ECG_f[90*sr:105*sr], EMG[90*sr:105*sr], pulso_f[90*sr:105*sr]]).reshape(4, 15000)  
 
-fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4)
-ax0.plot(ini[2,:])
-ax1.plot(ada[2,:])
-ax2.plot(eje[2,:])
-ax3.plot(fin[2,:])
-# plt.show()
+fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, constrained_layout=True)
+ax0.plot(ini[1,:]); ax0.set(ylabel='Inicial')
+ax1.plot(ada[1,:]); ax1.set(ylabel='Adaptacion')
+ax2.plot(eje[1,:]); ax2.set(ylabel='Ejercicio')
+ax3.plot(fin[1,:]); ax3.set(ylabel='Final')
+fig.suptitle('ECG para los 4 segmentos')
 
 # Dejar la señal en un intervalo entre [-1,1]
 iniree = (((ini-np.min(ini, axis=1)[:,np.newaxis])*2)/(np.max(ini-np.min(ini, axis=1)[:,np.newaxis], axis=1)[:,np.newaxis]))-1
@@ -61,31 +59,60 @@ adaree = (((ada-np.min(ada, axis=1)[:,np.newaxis])*2)/(np.max(ada-np.min(ada, ax
 ejeree = (((eje-np.min(eje, axis=1)[:,np.newaxis])*2)/(np.max(eje-np.min(eje, axis=1)[:,np.newaxis], axis=1)[:,np.newaxis]))-1
 finree = (((fin-np.min(fin, axis=1)[:,np.newaxis])*2)/(np.max(fin-np.min(fin, axis=1)[:,np.newaxis], axis=1)[:,np.newaxis]))-1
 fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, constrained_layout=True)
-ax0.plot(iniree[3,:]); ax0.set(ylabel='Respirograma')
-ax1.plot(adaree[3,:]); ax1.set(ylabel='ECG')
-ax2.plot(ejeree[3,:]); ax2.set(ylabel='EMG')
+ax0.plot(iniree[0,:]); ax0.set(ylabel='Respirograma')
+ax1.plot(adaree[1,:]); ax1.set(ylabel='ECG')
+ax2.plot(ejeree[2,:]); ax2.set(ylabel='EMG')
 ax3.plot(finree[3,:]); ax3.set(ylabel='Pulso')
 fig.suptitle('Reescalamiento de [-1,1] de segmento de actividad para 4 señales')
-# plt.show()
 
 #z-score, obtenemos la media y varianza con los estimadores
-def zscore(s):
-    media = (1/s.shape[1])*(np.sum(s, axis=1))
-    sigma = np.sqrt((1/(s.shape[1]-1))*(np.sum((s-media[:,np.newaxis])**2, axis=1)))
-    z     = (s-media[:,np.newaxis])/sigma[:,np.newaxis]
-    return z
-
-iniz = zscore(ini)
-adaz = zscore(ada)
-ejez = zscore(eje)
-finz = zscore(fin)
+iniz = MCI.zscore(ini)
+adaz = MCI.zscore(ada)
+ejez = MCI.zscore(eje)
+finz = MCI.zscore(fin)
 fig, (ax0,ax1,ax2,ax3) = plt.subplots(nrows=4, constrained_layout=True)
 ax0.plot(ejez[0,:]); ax0.set(ylabel='Respirograma')
 ax1.plot(ejez[1,:]); ax1.set(ylabel='ECG')
 ax2.plot(ejez[2,:]); ax2.set(ylabel='EMG')
 ax3.plot(ejez[3,:]); ax3.set(ylabel='Pulso')
 fig.suptitle('Reescalamiento zscore de segmento de actividad para 4 señales')
-# plt.show()
+
+#Histogramas para señal de respirograma sin reescalar
+nbins = int(round(np.sqrt(ini[0,:].shape[0])))
+bins0, hist0 = MCI.histograma(ini[0,:], nbins)
+bins1, hist1 = MCI.histograma(ada[0,:], nbins)
+bins2, hist2 = MCI.histograma(eje[0,:], nbins)
+bins3, hist3 = MCI.histograma(fin[0,:], nbins)
+fig,ax0 = plt.subplots(nrows=1, sharex=True)
+ax0.bar(bins0, hist0, width=0.05, alpha=0.3, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.3, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=0.3, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
+fig.suptitle('Histogramas del respirograma sin reescalar')
+
+#Histogramas para la señal de ECG sin reescalar
+bins0, hist0 = MCI.histograma(ini[1,:], nbins)
+bins1, hist1 = MCI.histograma(ada[1,:], nbins)
+bins2, hist2 = MCI.histograma(eje[1,:], nbins)
+bins3, hist3 = MCI.histograma(fin[1,:], nbins)
+fig,ax0 = plt.subplots(nrows=1, sharex=True)
+ax0.bar(bins0, hist0, width=0.05, alpha=0.3, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.3, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=0.3, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
+fig.suptitle('Histogramas del ECG sin reescalar')
+
+#Histogramas para la señal de EMG sin reescalar
+bins0, hist0 = MCI.histograma(ini[2,:], nbins)
+bins1, hist1 = MCI.histograma(ada[2,:], nbins)
+bins2, hist2 = MCI.histograma(eje[2,:], nbins)
+bins3, hist3 = MCI.histograma(fin[2,:], nbins)
+fig,ax0 = plt.subplots(nrows=1, sharex=True)
+ax0.bar(bins0, hist0, width=0.05, alpha=0.2, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.2, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=1, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
+fig.suptitle('Histogramas del EMG sin reescalar')
+
+#Histogramas para la señal de Onda de pulso sin reescalar
+bins0, hist0 = MCI.histograma(ini[3,:], nbins)
+bins1, hist1 = MCI.histograma(ada[3,:], nbins)
+bins2, hist2 = MCI.histograma(eje[3,:], nbins)
+bins3, hist3 = MCI.histograma(fin[3,:], nbins)
+fig,ax0 = plt.subplots(nrows=1, sharex=True)
+ax0.bar(bins0, hist0, width=0.05, alpha=0.3, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.3, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=1, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
+fig.suptitle('Histogramas de la señal de pulso sin reescalar')
 
 #Histogramas para señal de respirograma
 nbins = int(round(np.sqrt(ini[0,:].shape[0])))
@@ -95,7 +122,7 @@ bins2, hist2 = MCI.histograma(ejeree[0,:], nbins)
 bins3, hist3 = MCI.histograma(finree[0,:], nbins)
 fig,ax0 = plt.subplots(nrows=1, sharex=True)
 ax0.bar(bins0, hist0, width=0.05, alpha=0.3, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.3, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=0.3, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
-# plt.show()
+fig.suptitle('Histogramas del respirograma reescalado')
 
 #Histogramas para la señal de ECG
 bins0, hist0 = MCI.histograma(iniree[1,:], nbins)
@@ -104,7 +131,7 @@ bins2, hist2 = MCI.histograma(ejeree[1,:], nbins)
 bins3, hist3 = MCI.histograma(finree[1,:], nbins)
 fig,ax0 = plt.subplots(nrows=1, sharex=True)
 ax0.bar(bins0, hist0, width=0.05, alpha=0.3, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.3, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=0.3, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
-# plt.show()
+fig.suptitle('Histogramas del ECG reescalado')
 
 #Histogramas para la señal de EMG
 bins0, hist0 = MCI.histograma(iniree[2,:], nbins)
@@ -113,7 +140,7 @@ bins2, hist2 = MCI.histograma(ejeree[2,:], nbins)
 bins3, hist3 = MCI.histograma(finree[2,:], nbins)
 fig,ax0 = plt.subplots(nrows=1, sharex=True)
 ax0.bar(bins0, hist0, width=0.05, alpha=0.2, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.2, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=1, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
-# plt.show()
+fig.suptitle('Histogramas del EMG reescalado')
 
 #Histogramas para la señal de Onda de pulso
 bins0, hist0 = MCI.histograma(iniree[3,:], nbins)
@@ -122,7 +149,7 @@ bins2, hist2 = MCI.histograma(ejeree[3,:], nbins)
 bins3, hist3 = MCI.histograma(finree[3,:], nbins)
 fig,ax0 = plt.subplots(nrows=1, sharex=True)
 ax0.bar(bins0, hist0, width=0.05, alpha=0.3, label='Etapa inicial');ax0.bar(bins1, hist1, width=0.05, alpha=0.3, label='Adaptación');ax0.bar(bins2, hist2, width=0.05, alpha=1, label='Ejercicio');ax0.bar(bins3, hist3, width=0.05, alpha=0.3, label='Etapa final');plt.legend()
-# plt.show()
+fig.suptitle('Histogramas de la señal de pulso reescalada')
 
 # Obtenemos envolvente y área de la señal de EMG para sacar la correlación con otras señales
 def envolvente(signal):
@@ -146,47 +173,26 @@ fig, ax = plt.subplots()
 ax.plot(EMGenv)
 
 # Obtenemos segmentos de tiempo de las cuatro señales, la obtener su matriz de correación
-segt = 5
+segt = 3
+resp = MCI.epochs(Resp_f, segt, sr)
+ECG  = MCI.epochs(ECG_f, segt, sr)
+EMG  = MCI.epochs(EMGenv, segt, sr)
+pulso = MCI.epochs(pulso_f, segt, sr)
 
-def epochs(signal, segt, sr):
-    segt = segt*sr
-    i = 0
-    epochs = np.array([])
-    while(i<len(signal)-segt):
-        epochs = np.append(epochs, signal[i:i+segt])
-        i = i+segt
-    epochs = np.reshape(epochs, (int(len(signal)/segt),segt))
-    return epochs
+r1 = MCI.coef_corr(resp, ECG)
+r2 = MCI.coef_corr(resp, EMG)
+r3 = MCI.coef_corr(resp, pulso)
+r4 = MCI.coef_corr(ECG, EMG)
+r5 = MCI.coef_corr(ECG, pulso)
+r6 = MCI.coef_corr(EMG, pulso)
 
-def coef_corr(signal1, signal2):
-    r = np.array([])
-    for i in range(signal1.shape[0]):
-        S = np.cov(signal1[i,:], signal2[i,:])
-        z = S[0,1]/np.product( np.sqrt(S.diagonal() ) )
-        r = np.append(r, z)
-    return r 
-
-
-resp = epochs(Resp_f, segt, sr)
-ECG  = epochs(ECG_f, segt, sr)
-EMG  = epochs(EMGenv, segt, sr)
-pulso = epochs(pulso_f, segt, sr)
-
-r1 = coef_corr(resp, ECG)
-r2 = coef_corr(resp, EMG)
-r3 = coef_corr(resp, pulso)
-r4 = coef_corr(ECG, EMG)
-r5 = coef_corr(ECG, pulso)
-r6 = coef_corr(EMG, pulso)
-
-fig, ax = plt.subplots()
-ax.plot(r1, linewidth=3, label='resp-ECG')
-ax.plot(r2, linewidth=3, label='resp-EMG')
-ax.plot(r3, linewidth=3, label='resp-pulso')
-ax.plot(r4, linewidth=3, label='ECG-EMG')
-ax.plot(r5, linewidth=3, label='ECG-pulso')
-ax.plot(r6, linewidth=3, label='EMG-pulso')
-plt.legend(loc=3)
-plt.grid(True)
+fig, (ax0, ax1, ax2, ax3, ax4, ax5) = plt.subplots(nrows=6, constrained_layout=True)
+ax0.plot(r1); ax0.set(ylabel='resp-ECG')
+ax1.plot(r2); ax1.set(ylabel='resp-EMG')
+ax2.plot(r3); ax2.set(ylabel='resp-pulso')
+ax3.plot(r4); ax3.set(ylabel='ECG-EMG')
+ax4.plot(r5); ax4.set(ylabel='ECG-pulso')
+ax5.plot(r6); ax5.set(ylabel='EMG-pulso')
+fig.suptitle('Epocas vs Coeficiente de correlacion')
 plt.show()
 
